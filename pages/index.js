dbcfde2372083955a1dae8a5e451427853224d91
Mayu <satorimayu@gmail.com>
2022-05-03 19:02:21 -0300

    :sparkles: feat: correctly position button and add scroll

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,21 @@ import Layout from "../components/layout";
 import styles from "../styles/home.module.css"
 
 export default function Home() {
+
+  function scrollToelement() {
+    const element = document.getElementById('page2');
+    element.scrollIntoView({behavior: "smooth"});
+  };
+
   return (
     <Layout>
       <div className={styles.firstScreen}>
         <h2>Título</h2>
         <div className={styles.buttonBox}>
-          <button className={styles.arrowButton}>
+          <button
+           className={styles.arrowButton}
+           onClick={scrollToelement}
+          >
             <div className={styles.arrowBox}>
               <div className={styles.arrow} />
               <div className={styles.arrow} />
@@ -16,7 +25,7 @@ export default function Home() {
           </button>
         </div>
       </div>
-      <div className={styles.secondScreen}>
+      <div className={styles.secondScreen} id="page2">
         div2
       </div>
     </Layout>
